d849921ab7f9afea44bd6105ed009cb3ee1b179f
Lewis Gross <43077972+lewisgross1296@users.noreply.github.com>
2025-01-28 10:17:57 -0600

    Update openmc/deplete/abc.py
    Co-authored-by: Connor Moreno <camoreno@wisc.edu>

--- openmc/deplete/abc.py
+++ openmc/deplete/abc.py
@@ -635,22 +635,19 @@ class Integrator(ABC):
                 completed_timesteps = operator.prev_res.get_times()
                 completed_source_rates = operator.prev_res.get_source_rates()
                 num_previous_steps_run = len(completed_timesteps)
-                for step in range(num_previous_steps_run):
-                    if (
-                        timesteps[step] == completed_timesteps[step]
-                        and source_rates[step] == completed_source_rates[step]
-                    ):
-                        continue
-                    else:
-                        raise ValueError(
-                            "You are attempting to continue a run in which the previous results "
-                            "do not have the same initial steps as those provided to the "
-                            "Integrator. Please make sure you are using the correct timesteps,"
-                            "powers or power densities, and previous results file."
-                        )
-                seconds = seconds[num_previous_steps_run:]
-                source_rates = source_rates[num_previous_steps_run:]
-
+                if (
+                    np.equal(completed_timesteps, timesteps[: num_previous_steps_run]) and
+                    np.equal(completed_source_rates, source_rates[: num_previous_steps_run])
+                ):
+                    seconds = seconds[num_previous_steps_run:]
+                    source_rates = source_rates[num_previous_steps_run:]
+                else:
+                    raise ValueError(
+                        "You are attempting to continue a run in which the previous results "
+                        "do not have the same initial steps as those provided to the "
+                        "Integrator. Please make sure you are using the correct timesteps,"
+                        "powers or power densities, and previous results file."
+                    )
         self.timesteps = np.asarray(seconds)
         self.source_rates = np.asarray(source_rates)
 
